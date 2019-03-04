88d6b9c27a9790b72d041f274b3e4f33e5177a11
TakajiMesser <takaji.messer@gmail.com>
2019-03-03 23:00:21 -0800

    Improved logger

--- SpiceEngine/Rendering/PostProcessing/LogManager.cs
+++ SpiceEngine/Rendering/PostProcessing/LogManager.cs
@@ -1,61 +1,69 @@
-﻿using OpenTK;
-using OpenTK.Graphics.OpenGL;
-using SpiceEngine.Outputs;
-using SpiceEngine.Properties;
-using SpiceEngine.Rendering.Buffers;
-using SpiceEngine.Rendering.Processing;
-using SpiceEngine.Rendering.Shaders;
-using SpiceEngine.Rendering.Textures;
-using SpiceEngine.Rendering.Vertices;
-using System;
-using System.Collections.Concurrent;
-using System.Drawing;
-using System.Drawing.Text;
-using System.IO;
-using System.Linq;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace SpiceEngine.Rendering.PostProcessing
 {
     public class LogManager
     {
-        private static ConcurrentQueue<string> _lines = new ConcurrentQueue<string>();
+        public const int MAX_LINES_ON_SCREEN = 5;
+        public const int SYNC_FRAMES = 180;
 
-        private string _currentLogText;
-        private int _logFramesRemaining;
+        private static ConcurrentQueue<LogLine> _lineQueue = new ConcurrentQueue<LogLine>();
+        
+        private TextRenderer _textRenderer;
+        private LinkedList<LogLine> _screenLines = new LinkedList<LogLine>();
 
-        public int ClearFrameDelay { get; set; } = 1;
-
-        public bool TryGetLogText(out string text)
+        public LogManager(TextRenderer renderer)
         {
-            if (_currentLogText == null)
-            {
-                // Try to grab a new line
-                if (_lines.TryDequeue(out string line))
-                {
-                    _currentLogText = line;
-                    _logFramesRemaining = ClearFrameDelay;
-                }
-                else
-                {
-                    text = null;
-                    return false;
-                }
-            }
-
-            text = _currentLogText;
-
-            if (_logFramesRemaining > 1)
-            {
-                _logFramesRemaining--;
-            }
-            else
-            {
-                _currentLogText = null;
-            }
-
-            return true;
+            _textRenderer = renderer;
         }
 
-        public static void LogToScreen(string text) => _lines.Enqueue(text);
+        public void RenderToScreen()
+        {
+            var nOpenSlots = MAX_LINES_ON_SCREEN - _screenLines.Count;
+            var logLine = _screenLines.Last;
+
+            // Iterate through the list in reverse order, noting how many slots open up due to expiring items
+            for (var i = 0; i < _screenLines.Count; i++)
+            {
+                if (logLine == null)
+                {
+                    break;
+                }
+
+                _textRenderer.RenderText(logLine.Value.Text, 10, 10 + i * (5 + TextRenderer.GLYPH_HEIGHT));
+                logLine.Value.Increment();
+
+                var previous = logLine.Previous;
+
+                if (logLine.Value.IsExpired)
+                {
+                    // In this case, this line should be removed from the list, and we should note that we have an opening
+                    _screenLines.RemoveLast();
+                    nOpenSlots++;
+                }
+
+                logLine = previous;
+            }
+
+            // For each opening, we should try to get a line from the static line queue and place it at the front of the list
+            for (var i = 0; i < nOpenSlots; i++)
+            {
+                if (_lineQueue.TryDequeue(out LogLine line))
+                {
+                    _screenLines.AddFirst(line);
+                }
+            }
+        }
+
+        public static void LogToScreen(string text)
+        {
+            var line = new LogLine(text)
+            {
+                ClearFrameDelay = 180
+            };
+
+            _lineQueue.Enqueue(line);
+        }
     }
 }
